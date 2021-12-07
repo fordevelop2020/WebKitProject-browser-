b056cf09e42d7ac610a637805fae43eefa54fd75
fordevelop2020 <fordevelop2020@gmail.com>
2021-12-07 11:45:24 +0100

    Initial Commit

---
+++ Project04/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Project04
+//
+//  Created by omari on 7/12/2021.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webKit = WKWebView()
+    
+    override func loadView() {
+        webKit = WKWebView()
+        webKit.navigationDelegate = self
+        view = webKit
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webKit.load(URLRequest(url: url))
+        webKit.allowsBackForwardNavigationGestures = true
+    }
+    
+    @objc func openTapped(){
+        let vc = UIAlertController(title: "Open page ..",message: nil, preferredStyle: .actionSheet)
+        vc.addAction(UIAlertAction(title: "www.hackingwithswift.com", style: .default, handler: openPage))
+        vc.addAction(UIAlertAction(title: "www.apple.com", style: .default, handler: openPage))
+        vc.addAction(UIAlertAction(title: "www.facebook.com", style: .default, handler: openPage))
+        vc.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+     
+    func openPage(action : UIAlertAction){
+        let url = URL(string: "https://" + action.title!)!
+        webKit.load(URLRequest(url: url))
+        webKit.allowsBackForwardNavigationGestures = true
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
+    
+
+}
+
